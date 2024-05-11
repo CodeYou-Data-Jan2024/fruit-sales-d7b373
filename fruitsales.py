405da6ef7b94ef69ef9944d80785a9b931763b28
TroyHeizer <TroyHeizer@gmail.com>
2024-05-10 23:16:30 -0400

    first commit

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,14 @@
-# add your code here
+import pandas as pd
+
+# Create dictionary with the fruit sales data
+data = {
+    'Apples': [35, 41],
+    'Bananas': [21, 34]
+}
+
+# Create a Data Frame from the dictionary
+df = pd.DataFrame(data, index=['2017 Sales', '2018 Sales'])
+
+# Write the DataFrame to a CSV file
+df.to_csv('fruit.csv')
+
